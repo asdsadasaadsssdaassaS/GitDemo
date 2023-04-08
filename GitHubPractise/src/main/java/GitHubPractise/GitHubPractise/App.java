10741f26afd4653270e3076a64344ab539849bac
srinath <jonnada.srinathmudiraj@gmail.com>
2023-04-08 12:43:12 +0530

    Commite on 08 April

---
+++ GitHubPractise/src/main/java/GitHubPractise/GitHubPractise/App.java
@@ -0,0 +1,13 @@
+package GitHubPractise.GitHubPractise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
